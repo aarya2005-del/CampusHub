aba1cc1bf0fb118707ed293a4c4dfb262d56377f
Aarya <aaryabhavsar16@gmail.com>
2026-07-10 13:55:26 +0530

    Day 2: Created auth routes and controllers

---
+++ server/controllers/authController.js
@@ -0,0 +1,11 @@
+exports.register = (req, res) => {
+  res.json({
+    message: "Register API Working",
+  });
+};
+
+exports.login = (req, res) => {
+  res.json({
+    message: "Login API Working",
+  });
+};
--- server/index.js
+++ server/index.js
@@ -1,32 +1,27 @@
-console.log("STEP 1");
-
 require('dotenv').config();
 
-console.log("STEP 2");
-
 const express = require('express');
-
-console.log("STEP 3");
-
 const cors = require('cors');
-
-console.log("STEP 4");
-
 const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+console.log(authRoutes);
 
-console.log("STEP 5");
+// connectDB();
 
 const app = express();
+console.log("Express app created");
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.json({ message: 'CampusHub API is running' });
 });
 
 const PORT = process.env.PORT || 5000;
-
+console.log("About to listen...");
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
